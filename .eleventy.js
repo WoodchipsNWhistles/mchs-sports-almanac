a52620fc01def5caf115d92bd24873c4956eb353
WoodchipsNWhistles <dave.mitchner@gmail.com>
2026-01-25 12:07:18 -0500

    Launch rough draft site shell (styled homepage + GWBB landing)

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
 module.exports = function (eleventyConfig) {
+  // Copy static assets straight through to /docs/assets
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
   return {
     pathPrefix: "/mchs-sports-almanac/",
     dir: {
       input: "src",
-      output: "docs"
+      output: "docs",
+      includes: "_includes"
     }
   };
 };
